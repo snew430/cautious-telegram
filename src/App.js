858f640e902c0a9a2acf5de651dae12948cae5db
snew430 <seanpnew@hotmail.com>
2022-04-16 08:46:07 -0400

    useeffect and esestate on nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
+    const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0])
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav 
+      categories = {categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}/>
       <main>
-        <About></About>
+        <Gallery />
+        <About />
       </main>
     </div>
   );
