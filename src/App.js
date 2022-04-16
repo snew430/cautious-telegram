6d41a1e8f1430a8329808abbf9f26e738ca513bd
snew430 <seanpnew@hotmail.com>
2022-04-16 10:00:49 -0400

    track form data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
-import Gallery from './components/Gallery';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
-    const [categories] = useState([
+  const [categories] = useState([
     {
       name: "commercial",
       description:
@@ -18,16 +19,18 @@ function App() {
     },
   ]);
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0])
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
-      <Nav 
-      categories = {categories}
-      setCurrentCategory={setCurrentCategory}
-      currentCategory={currentCategory}/>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      />
       <main>
-        <Gallery currentCategory={currentCategory}/>
+        <ContactForm />
+        <Gallery currentCategory={currentCategory} />
         <About />
       </main>
     </div>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
   return (
     <section>
       <h1>Contact</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="email">Email Address:</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <input type="message" rows="5" />
+          <input
+            type="message"
+            rows="5"
+            defaultValue={message}
+            onChange={handleChange}
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
